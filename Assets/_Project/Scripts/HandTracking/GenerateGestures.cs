6c1dc69be55c144b2c21fd674dd16fa200f941cd
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2023-06-07 17:47:38 +1000

    add sample script with appropriate namespacing

---
+++ Assets/_Project/Scripts/HandTracking/GenerateGestures.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SoggyInkGames.Equanimous.Lab.HandTracking //namespace ends with folder name
+{
+    public class GenerateGestures : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        // todo: add code
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
