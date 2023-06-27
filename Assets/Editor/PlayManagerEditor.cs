7f0f03e10b490bcc901cbb7c2d9b9af58c7a2aad
Anai Yusary Araya Rocha <anai.yusary@gmail.com>
2023-06-27 10:52:56 +1000

    hook up UXML to Playmanager editor to instantiate visual elements WIP

--- Assets/Editor/PlayManagerEditor.cs
+++ Assets/Editor/PlayManagerEditor.cs
@@ -10,15 +10,19 @@ namespace SoggyInkGames.Equanimous.Lab.Managers
     [CustomEditor(typeof(PlayManager))]
     public class PlayManagerEditor : Editor
     {
-        // public VisualTreeAsset m_UXML;
+        public VisualTreeAsset m_UXML;
 
         public override VisualElement CreateInspectorGUI()
         {
             var root = new VisualElement();
+            m_UXML.CloneTree(root);
 
             // Draw the default inspectror
-
-            var foldout = new Foldout() { viewDataKey = "TestingCustomInspectorFoldout", text = "Full Inspector"};
+            var foldout = new Foldout() 
+            {
+                viewDataKey = "TestingCustomInspectorFoldout", //giving it a viewdatakey will let the foldout retain state between domain reloads, closing/opening windows, otherise it will be recreated with the default state expanded.
+                text = "Full Inspector"
+            };
             InspectorElement.FillDefaultInspector(foldout, serializedObject, this);
             root.Add(foldout);
             return root;
--- Assets/_Project/Scripts/Managers/PlayManager.cs
+++ Assets/_Project/Scripts/Managers/PlayManager.cs
@@ -7,6 +7,11 @@ namespace SoggyInkGames.Equanimous.Lab.Managers
     [RequireComponent(typeof(UIDocument))]
     public class PlayManager : MonoBehaviour
     {
+        [Header("This is a Header")]
+        public float m_ElementRandomRange = 20;
+        [Range(10, 40)] public float m_ElementForce = 25;
+        public int m_ElementCount = 10;
+        public float m_ElementDelay = 0.1f;
         public GameObject m_ElementPrefab;
 
         public ElementManager[] m_Elements;
